c891e063e11a837ca15019cadb73cc3e5f894a3b
Eric Choi <emchoi91@gmail.com>
2021-11-15 11:13:46 -0800

    add resume to site

--- features/NavBar.tsx
+++ features/NavBar.tsx
@@ -3,10 +3,21 @@ import styles from '../styles/NavBar.module.css';
 function NavBar(): JSX.Element {
   return (
     <nav className={styles.navbar}>
-      <a className={styles.link} href="#about">About</a>
-      <a className={styles.link} href="#skills">Skills</a>
-      <a className={styles.link} href="#projects">Projects</a>
-      <a className={styles.link} href="#contact">Contact</a>
+      <a className={styles.link} href="#about">
+        About
+      </a>
+      <a className={styles.link} href="#skills">
+        Skills
+      </a>
+      <a className={styles.link} href="#projects">
+        Projects
+      </a>
+      <a className={styles.link} href="/resume.pdf">
+        Resume
+      </a>
+      <a className={styles.link} href="#contact">
+        Contact
+      </a>
     </nav>
   );
 }
